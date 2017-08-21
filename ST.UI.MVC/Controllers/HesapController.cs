029db56c1f5dc1f3ee350fa0243931f202c27bc3
Mesut OZTURK <mesutoztrk@msn.com>
2017-08-21 13:34:27 +0300

    account controller

---
+++ ST.UI.MVC/Controllers/HesapController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ST.BLL.Account;
+using ST.Models.IdentityModels;
+using ST.Models.ViewModels;
+using static ST.BLL.Account.MembershipTools;
+
+namespace ST.UI.MVC.Controllers
+{
+    public class HesapController : Controller
+    {
+        // GET: Hesap
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Kayit(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var userManager = NewUserManager();
+            var checkUser = userManager.FindByEmail(model.Email);
+            if (checkUser != null)
+            {
+                ModelState.AddModelError("", "Bu email adresi kullanılmaktadır");
+                return View(model);
+            }
+            checkUser = await userManager.FindByNameAsync(model.Username);
+            if (checkUser != null)
+            {
+                ModelState.AddModelError("", "Bu kullanici adı kullanılmaktadır");
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                Surname = model.Surname,
+                UserName = model.Username
+            };
+            bool adminMi = userManager.Users.Count() == 0;
+            var sonuc = await userManager.CreateAsync(user, model.ConfirmPassword);
+            if (sonuc.Succeeded)
+            {
+                if (adminMi)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+                else
+                {
+                    if (model.FirmaMi)
+                        userManager.AddToRole(user.Id, "Firma");
+                    else
+                        userManager.AddToRole(user.Id, "Musteri");
+                }
+
+                return RedirectToAction("Index", "Ana");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı kayıt işleminde hata oluştu!");
+                return View(model);
+            }
+        }
+
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Giris(LoginViewModel model)
+        {
+
+            return View();
+        }
+    }
+}
