0fa2fe329c3e388599185175a699812e73784eba
Mesut OZTURK <mesutoztrk@msn.com>
2017-08-23 13:47:53 +0300

    delete

--- ST.UI.MVC/Areas/Yonetim/Controllers/UrunKategoriController.cs
+++ ST.UI.MVC/Areas/Yonetim/Controllers/UrunKategoriController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace ST.UI.MVC.Areas.Yonetim.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UrunKategoriController : Controller
     {
         // GET: Yonetim/UrunKategori
@@ -55,6 +56,58 @@ namespace ST.UI.MVC.Areas.Yonetim.Controllers
                 KategoriAdi = x.KategoriAdi
             }).ToList();
             return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Guncelle(UrunKategoriViewModel model)
+        {
+            try
+            {
+                var kategori = new UrunKategoriRepo().GetById(model.Id);
+                kategori.KategoriAdi = model.KategoriAdi;
+                kategori.Aciklama = model.Aciklama;
+                new UrunKategoriRepo().Update();
+                var data = new
+                {
+                    success = true,
+                    message = "Kategori Güncelleme işlemi başarılı"
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                var data = new
+                {
+                    success = false,
+                    message = "Kategori Güncelleme işlemi başarısız: " + ex.Message
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Sil(int? id)
+        {
+            try
+            {
+                var kategori = new UrunKategoriRepo().GetById(id.Value);
+                new UrunKategoriRepo().Delete(kategori);
+                var data = new
+                {
+                    success = true,
+                    message = "Kategori Silme işlemi başarılı"
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                var data = new
+                {
+                    success = false,
+                    message = "Kategori Silme işlemi başarısız :" + ex.Message
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
         }
         #endregion
     }
--- ST.UI.MVC/Scripts/app.js
+++ ST.UI.MVC/Scripts/app.js
@@ -23,12 +23,32 @@ app.factory("api", function ($http) {
             }).then(function (response) {
                 success(response.data);
             });
+        },
+        urunkategoriguncelle: function (model, success) {
+            $http({
+                url: apiurl + 'UrunKategori/Guncelle',
+                data: model,
+                method: 'POST',
+                dataType: 'JSON'
+            }).then(function (response) {
+                success(response.data);
+            });
+        },
+        urunkategorisil: function (id, success) {
+            $http({
+                url: apiurl + 'UrunKategori/Sil/' + id,
+                method: 'POST',
+                dataType: 'JSON'
+            }).then(function (response) {
+                success(response.data);
+            });
         }
     }
 });
 
 app.controller("UrunKategoriCtrl",
     function ($scope, api) {
+        $scope.kategori = {};
         $scope.kategoriler = [];
         $scope.duzenlenecek = null;
         function init() {
@@ -37,17 +57,38 @@ app.controller("UrunKategoriCtrl",
             });
         }
         $scope.ekle = function () {
+            console.log($scope.kategori);
             api.urunkategoriekle($scope.kategori,
                 function (data) {
-                    $scope.kategoriler.KategoriAdi = "";
-                    $scope.kategoriler.Aciklama = "";
                     init();
                     console.log(data);
                     alert(data.message);
                 });
         }
-        $scope.duzenle=function(kategori) {
-            $scope.duzenlenecek = kategori;
+        $scope.duzenle = function (kategori) {
+            $scope.duzenlenecek = {
+                Id: kategori.Id,
+                KategoriAdi: kategori.KategoriAdi,
+                Aciklama: kategori.Aciklama
+            };
+        }
+        $scope.guncelle = function () {
+            api.urunkategoriguncelle($scope.duzenlenecek,
+                function (data) {
+                    console.log(data);
+                    init();
+                    alert(data.message);
+                    if (data.success === true)
+                        $scope.duzenlenecek = null;
+                });
+        }
+        $scope.sil = function (id) {
+            api.urunkategorisil(id,
+                function (data) {
+                    console.log(data);
+                    init();
+                    alert(data.message);
+                });
         }
         init();
     });
